3e15475767dd9ff56a0e82d3e86ba81d46c5456d
dnbravo04 <digobravo@outlook.com>
2023-05-29 16:12:14 -0500

    (finally) fixed navbar

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,28 +1,35 @@
-function navbar() {
-    var x = document.getElementById("links")
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
-
-var fname = document.getElementById("name")
-var phone = document.getElementById("phone")
-var email = document.getElementById("email")
-var message = document.getElementById("message")
-var contact = document.getElementsByName("contact")
-
-submitData.addEventListener("click", function(event){ 
-    event.preventDefault()
-    var data = {
-        "name":fname.value,
-        "phone":phone.value,
-        "email":email.value,
-        "message":message.value,
-        "contact":contact.value
-    }
-    console.log(JSON.stringify(data))
+burguerIcon.addEventListener("click", function (event) {
+  let x = document.getElementById("links");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 });
 
+window.addEventListener("resize", function () {
+    let element = this.document.getElementById("links");
+    if (window.innerWidth < 700){
+        element.style.display = 'none';
+    } else{
+        element.style.display = 'block';
+    }
+});
 
+var fname = document.getElementById("name");
+var phone = document.getElementById("phone");
+var email = document.getElementById("email");
+var message = document.getElementById("message");
+var contact = document.getElementsByName("contact");
+
+submitData.addEventListener("click", function (event) {
+  event.preventDefault();
+  var data = {
+    name: fname.value,
+    phone: phone.value,
+    email: email.value,
+    message: message.value,
+    contact: contact.value,
+  };
+  console.log(JSON.stringify(data));
+});
